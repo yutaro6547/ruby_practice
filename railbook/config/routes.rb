b3d8b97c0263e690fb6bc170151734ef0fd83f92
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2017-07-09 17:13:55 +0900

    動作確認のためいったんコメントアウト

--- railbook/config/routes.rb
+++ railbook/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'hello/view'
-  get 'hello/list'
+  # get 'hello/view'
+  # get 'hello/list'
 end
